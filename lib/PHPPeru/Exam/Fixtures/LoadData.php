a13a4a4cc74f2a3d789d07350c43dc225960fc3d
Luis Cordova <cordoval@gmail.com>
2012-03-18 02:23:10 -0500

    added fixture empty class

---
+++ lib/PHPPeru/Exam/Fixtures/LoadData.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+ * This file is part of the PHPPeru packages.
+ *
+ * (c) Luis Cordova <cordoval@gmail.com>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace PHPPeru\Exam\Fixtures;
+
+use PHPPeru\Exam\SimpleExam;
+
+/**
+ * LoadData.php: Short description.
+ *
+ * Two line explanation.
+ *
+ * @author Luis Cordova <cordoval@gmail.com>
+ */
+class LoadData
+{
+
+}
