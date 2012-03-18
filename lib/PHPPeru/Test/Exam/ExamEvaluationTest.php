f80d03718f15ba2380068517e2cb787d314eed45
Luis Cordova <cordoval@gmail.com>
2012-03-18 00:07:00 -0500

    adde test class for testing Exam evaluation

---
+++ lib/PHPPeru/Test/Exam/ExamEvaluationTest.php
@@ -0,0 +1,29 @@
+<?php
+namespace PHPPeru\Test\Exam;
+
+use PHPPeru\Test\Exam;
+
+/**
+ * Description of ExamEvaluationTest
+ *
+ * @author Luis Cordova <cordoval@gmail.com>
+ */
+class ExamEvaluationTest extends \PHPUnit_Framework_TestCase {
+    //put your code here
+
+    protected $step;
+    
+    public function setUp(){
+       
+    }
+    
+    public function tearDown(){
+        $this->step = null;
+    }
+        
+    public function testSomething()
+    {   
+
+    }
+    
+}
